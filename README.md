4fe9426c19b984922d8481a0006b482d17b986e0
xBartolinix <wertownia2000@gmail.com>
2021-04-21 22:35:31 +0200

    Add README.md, remove test code from index.js, change name in package.json, updated pug to 3.0.2, added background-color in style.css, beautify if statements for easier readability in gallery.js

---
+++ README.md
@@ -0,0 +1,18 @@
+# Uploader-Backend
+
+~~Not~~ Simple express file uploader.
+
+### Example dotenv config:
+```dotenv
+client_id=
+client_secret=
+SES_SECRET=
+cookie_key=
+test_token=
+PORT=3000
+DB_PASSWORD=
+PASS_SALT_ROUNDS=10
+TOKEN_SALT_ROUNDS=5
+TOKEN_SECRET=
+DB_URI=
+```
--- routes/api/gallery.js
+++ routes/api/gallery.js
@@ -9,7 +9,7 @@ const {userToken} = require('../../midlewares/checkTokens');
 router.delete('/:userid/:filename/delete', userToken, async function(req, res, next){
     const user = req.user;
     if(!req.params.userid|| !req.params.filename) return res.send('nope')
-    if(req.params.userid!=user._id) return res.status(403).type('json').send({status: 403, message: "No permission"});
+    if(req.params.userid!==user._id) return res.status(403).type('json').send({status: 403, message: "No permission"});
     File.findOne({filename: req.params.filename, uploaderId: user._id}, async (err, file)=>{
         if(err) return res.status(500).type('json').send({status: 500, message: "An error has occurred"});
         if(!file) return res.status(404).type('json').send({status: 404, message: "File not found"});
@@ -34,10 +34,10 @@ router.get('/list', async function(req, res, next){
     if(!req.query.username) return res.status(400).render('error', {message: "No user specified", error: {status: 400}});
     let limit,sort;
     if(!req.query.limit || req.query.limit<10) limit=10;
-    else if(req.query.limit>100) limit=100;
-    else limit=Number(req.query.limit);
+        else if(req.query.limit>100) limit=100;
+        else limit=Number(req.query.limit);
     if(!req.query.sort || req.query.sort<=-1) sort=-1;
-    else sort=1;
+        else sort=1;
     User.findOne({username: req.query.username},async (err, user)=>{
         if(err) return res.status(500).type('json').send({status: 500, message: "An error has occurred"});
         if(!user) return res.status(404).type('json').send({status: 404, message: "User not found"});
